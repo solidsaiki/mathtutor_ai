16361d01e1530741f44da34116482df7632648d2
solidsaiki <132334871+solidsaiki@users.noreply.github.com>
2023-08-03 08:51:39 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from streamlit_card import card
+
+st.markdown("# マスト アイ")
+st.markdown("### Stanford大学を目指す数学の家庭教師")
+
+st.image("https://storage.googleapis.com/www.sinfonia-ai.com/mathtutor-ai/images/700815b4.jpg")
+
+st.markdown("##### Stanford大学でコンピュータサイエンスを専攻し、学位の取得を目指しましょう")
+
+st.write(
+"未来を変える **汎用人工知能(AGI)** に挑戦するために,"\
+"Stanford大学のコンピュータサイエンス専攻で学位を取得し,"\
+"世界トップクラスの人工知能研究機関であるスタンフォード人工知能研究所で,"\
+"人工知能の理論を極め, **AGIの研究・開発に従事する研究者** になっていただきます."
+)
+st.write("そのために,当レッスンでは,生徒様に **Stanford大学のコンピュータサイエンス専攻の入学** を目指していただきます.")
+
