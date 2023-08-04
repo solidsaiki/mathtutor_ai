4a7bf41b711dd16c3662e7af08462abe845a2070
solidsaiki <132334871+solidsaiki@users.noreply.github.com>
2023-08-04 16:56:09 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -2,51 +2,68 @@ import streamlit as st
 from streamlit_card import card
 
 st.markdown("# マスト アイ")
-st.markdown("### Stanford大学を目指す数学の家庭教師")
 
 st.image("https://storage.googleapis.com/www.sinfonia-ai.com/mathtutor-ai/images/700815b4.jpg")
 
+st.markdown("### Stanford大学を目指す数学の家庭教師")
+
 st.markdown("##### Stanford大学でコンピュータサイエンスを専攻し、学位の取得を目指しましょう")
+
+st.image("https://storage.googleapis.com/www.sinfonia-ai.com/mathtutor-ai/images/stanford.jpg")
 
 st.markdown("""
 未来を変える **汎用人工知能(AGI)** の開発に挑戦するために、
 Stanford大学のコンピュータサイエンス専攻で学位を取得し、
-世界トップクラスの人工知能研究機関であるスタンフォード人工知能研究所で、
-人工知能の理論を極め、 **AGIの研究・開発に従事する研究者** になりましょう。  
+世界トップクラスの人工知能研究機関であるStanford人工知能研究所で、
+人工知能の理論を極め、 **AGIの研究・開発に従事する研究者** になりましょう。
 """)
 
 st.markdown("""
-そのために、当レッスンでは、生徒様に **Stanford大学のコンピュータサイエンス専攻の入学** を目指していただきます。  
+そのために、当レッスンでは、生徒様に **Stanford大学のコンピュータサイエンス専攻の入学** を目指していただきます。
 """)
 
+st.markdown("##### AP(Advanced Placement)のMath & Computer Science")
+
 st.markdown("""
-コンピュータサイエンス専攻では、AP(Advanced Placement)のMath & Computer Scienceが必須で、
+Stanford大学のコンピュータサイエンス専攻では、AP(Advanced Placement)のMath & Computer Scienceが必須で、  
 AP Calculus BC  
 AP Statistics  
 AP Computer Science A  
 において、評価５で取得することが必要です。  
-当レッスンでは、AP(Advanced Placement)のMath & Computer Scienceのレッスンを行っており、評価５が取れるように指導します。
+当レッスンでは、AP(Advanced Placement)のMath & Computer Scienceのレッスンを行っており、評価５が取れるように指導します。  
 """)
 
-st.markdown("""
-当レッスンでは、小学生から高校生まで、幅広い年齢の生徒様に対して、Stanford大学のコンピュータサイエンス専攻の入学を目指したレッスンを行っています。
-""")
+st.markdown("##### Application Requirements")
 
 st.markdown("""
-なので、どの年齢からでもレッスンを受けることができ、その年齢からStanford大学の選考までの最適なスケジュールを作成し、
-そのスケジュールに沿ったレッスンを行います。  
+Application Requirementsでは、「Essay or Personal Statement」が必須ですが、
+当レッスンでの人工知能の学習内容によって「Essay or Personal Statement」を記述することが出来ます。
 """)
 
+st.markdown("##### グーグルコラボでパイソンのプログラムを駆使し、数学を分かりやすく解説")
+
 st.markdown("""
-テキストはグーグルコラボで独自に作成しています。  
+パイソンは、読みやすく簡潔に書けるプログラミング言語で、機械学習やウェブアプリケーションに使われます。  
 グーグルコラボでは、パイソンのプログラムを実行することができます。  
+テキストはグーグルコラボで独自に作成しています。  
 数学をより深く理解するために、数学の問題をパイソンのプログラムで解き、プログラミングの能力を育てます。  
+""")
+
+st.markdown("##### Stanford大学のコンピュータサイエンス専攻の入学を目指したレッスン")
+
+st.markdown("""
+当レッスンでは、小学生から高校生まで、幅広い年齢の生徒様に対して、Stanford大学のコンピュータサイエンス専攻の入学を目指したレッスンを行っています。  
+どの年齢からでもレッスンを受けることができ、その年齢からStanford大学の選考までの最適なスケジュールを作成し、
+そのスケジュールに沿ったレッスンを行います。
 """)
 
 st.markdown("##### 小学生低学年のスケジュール")
 
+st.image("https://storage.googleapis.com/www.sinfonia-ai.com/mathtutor-ai/images/p13.png")
+
 st.markdown("""
-生徒さんの個性・理解度に合わせて、教科書を中心にした先取り学習を行い、小学３年生の３学期末までには、小学６年間の算数を修了し、中学数学に取り掛かります。
+生徒さんの個性・理解度に合わせて、書画カメラを使用し、教科書を中心にした先取り学習を行い、
+小学３年生の３学期末までには、小学６年間の算数を修了し、中学数学に取り掛かります。
 """)
 
 st.markdown("##### 小学生高学年のスケジュール")
@@ -60,7 +77,9 @@ st.markdown("""
 st.markdown("##### 中学生のスケジュール")
 
 st.markdown("""
-中学３年の３学期末までに、中学数学と高校の数学１・数学A・数学２・数学B・数学３・数学Cの修了をめざします。
+中学３年の３学期末までに、中学数学と高校の数学１・数学A・数学２・数学B・数学３・数学Cの修了をめざします。  
+AP(Advanced Placement)のMathの基礎を修得します。  
+**AP Computer Science A**において、評価５を取得を目指します。  
 """)
 
 st.markdown("##### 高校生のスケジュール")
@@ -70,7 +89,12 @@ AP(Advanced Placement)のMath & Computer Scienceの
 AP Calculus BC  
 AP Statistics  
 AP Computer Science A  
-において、評価５を取得するように指導します。
+において、評価５を取得するように指導します。  
+**chatGPT** などの生成AIの基礎となるニューラルネットワークと生成AIの核となるトランスフォーマーの数学理論を理解し、
+ニューラルネットワークとトランスフォーマーを既存のプログラムコードを一切使わずにゼロからプログラムの作成し、実装を行います。
+これにより、フレームワークとして使用するPyTorchをより深く理解でき、完璧に使いこなすことができるようにまります。
+そして、PyTorchをベースに、chatGPTなどの生成AIの実装を行います。  
+これらの人工知能の学習によって、Application Requirementsでは、「Essay or Personal Statement」を記述することができます。
 """)
 
 st.markdown("##### 料金")
